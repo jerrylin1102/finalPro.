1e4995ad04dbecb216420856efc14fca09ec3182
LGJ199 <rabbit930326@gmail.com>
2023-12-26 00:00:39 +0800

    reset

--- demo/src/main/java/com/example/demo/setting.java
+++ demo/src/main/java/com/example/demo/setting.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 import android.accounts.Account;
+import android.app.AlertDialog;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,7 +13,9 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -53,7 +57,6 @@ public class setting extends AppCompatActivity {
         imgBack = findViewById(R.id.imgBack);
         imgBack.setOnClickListener(lis);
 
-        btn_Account.setOnClickListener(accountlistener);
         btn_Email.setOnClickListener(emaillistener);
         btn_Username.setOnClickListener(usernamelistener);
         btn_Password.setOnClickListener(passwordlistener);
@@ -76,7 +79,12 @@ public class setting extends AppCompatActivity {
                         btn_Username.setText(username);
                         String password = userSnapshot.child("password").getValue(String.class);
                         btn_Password.setText(password);
-                        Log.e("ER", userData.getAccount());
+
+                        userData userData1 = com.example.demo.userData.getInstance();
+                        userData1.setName(btn_Username.getText().toString());
+                        userData1.setEmail(btn_Email.getText().toString());
+                        userData1.setAccount(btn_Account.getText().toString());
+                        userData1.setPw(btn_Password.getText().toString());
                     }
                 }
             }
@@ -96,31 +104,138 @@ public class setting extends AppCompatActivity {
         }
     };
 
-    private View.OnClickListener accountlistener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            finish();
-        }
-    };
-
     private View.OnClickListener passwordlistener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            finish();
+            AlertDialog.Builder reset = new AlertDialog.Builder(setting.this);
+            RelativeLayout layout = new RelativeLayout(setting.this);
+            final EditText input = new EditText(setting.this);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.MATCH_PARENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            input.setLayoutParams(params);
+
+
+            // 设置EditText的ID，以便设置相对位置
+            input.setId(View.generateViewId());
+
+            layout.addView(input);
+
+            reset.setTitle("更改視窗");
+            reset.setIcon(R.mipmap.reset);
+            reset.setView(layout);
+            reset.setMessage("新的密碼：");
+            reset.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String newPassword = input.getText().toString();
+                    userData userData = com.example.demo.userData.getInstance();
+                    userData.setPw(newPassword);
+                    btn_Password.setText(userData.getPw());
+
+                    HelperClass helperClass = new HelperClass(userData.getAccount(), userData.getEmail(), userData.getName(), userData.getPw());
+                    reference.child(userData.getAccount()).setValue(helperClass);
+                }
+            });
+            reset.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            reset.show();
         }
     };
 
     private View.OnClickListener usernamelistener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            finish();
+            AlertDialog.Builder reset = new AlertDialog.Builder(setting.this);
+            RelativeLayout layout = new RelativeLayout(setting.this);
+            final EditText input = new EditText(setting.this);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.MATCH_PARENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            input.setLayoutParams(params);
+
+
+            // 设置EditText的ID，以便设置相对位置
+            input.setId(View.generateViewId());
+
+            layout.addView(input);
+
+            reset.setTitle("更改視窗");
+            reset.setIcon(R.mipmap.reset);
+            reset.setView(layout);
+            reset.setMessage("新的暱稱：");
+            reset.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String newusername = input.getText().toString();
+                    userData userData = com.example.demo.userData.getInstance();
+                    userData.setName(newusername);
+                    btn_Username.setText(userData.getName());
+
+                    HelperClass helperClass = new HelperClass(userData.getAccount(), userData.getEmail(), userData.getName(), userData.getPw());
+                    reference.child(userData.getAccount()).setValue(helperClass);
+                }
+            });
+            reset.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            reset.show();
         }
     };
 
     private View.OnClickListener emaillistener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            finish();
+            AlertDialog.Builder reset = new AlertDialog.Builder(setting.this);
+            RelativeLayout layout = new RelativeLayout(setting.this);
+            final EditText input = new EditText(setting.this);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.MATCH_PARENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            input.setLayoutParams(params);
+
+
+            // 设置EditText的ID，以便设置相对位置
+            input.setId(View.generateViewId());
+
+            layout.addView(input);
+
+            reset.setTitle("更改視窗");
+            reset.setIcon(R.mipmap.reset);
+            reset.setView(layout);
+            reset.setMessage("新的email：");
+            reset.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String newemail = input.getText().toString();
+                    userData userData = com.example.demo.userData.getInstance();
+                    userData.setEmail(newemail);
+                    btn_Email.setText(userData.getEmail());
+
+                    HelperClass helperClass = new HelperClass(userData.getAccount(), userData.getEmail(), userData.getName(), userData.getPw());
+                    reference.child(userData.getAccount()).setValue(helperClass);
+                }
+            });
+            reset.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            reset.show();
         }
     };
 }
