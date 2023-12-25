08106570c7a8a98c053bb1e133859cb9aec8fb9f
jkl921102 <jkl921102@gmail.com>
2023-12-25 10:55:16 +0800

    fragment_home.xml layout

--- demo/src/main/java/com/example/demo/diet_inspection.java
+++ demo/src/main/java/com/example/demo/diet_inspection.java
@@ -1,4 +1,15 @@
 package com.example.demo;
 
-public class diet_inspection {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class diet_inspection extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diet_inspection);
+
+    }
 }
