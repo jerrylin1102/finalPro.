8b9e16f27cfa823bd0d7dbed2e8fa970b0099bef
jerrylin <jkl921102@gmail.com>
2023-12-25 17:10:28 +0800

    setting build

--- demo/src/main/java/com/example/demo/setting.java
+++ demo/src/main/java/com/example/demo/setting.java
@@ -1,17 +1,32 @@
 package com.example.demo;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class setting extends AppCompatActivity {
     private ImageView imgBack;
+    private TextView show;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
     FragmentManager fragmentManager = getFragmentManager();
     @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +39,33 @@ public class setting extends AppCompatActivity {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(ContextCompat.getColor(setting.this, android.R.color.holo_orange_light));
 
+            show =  this.findViewById(R.id.show);
             imgBack=findViewById(R.id.imgBack);
             imgBack.setOnClickListener(lis);
+
+            firebaseDatabase = FirebaseDatabase.getInstance();
+            databaseReference = firebaseDatabase.getReference("users");
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                        // 檢查使用者的名字是否為 "Jerry"
+                        String userName = userSnapshot.child("name").getValue(String.class);
+                        if ("jerry".equals(userName)) {
+                            // 如果名字為 "Jerry"，則取得該使用者的密碼
+                            String email = userSnapshot.child("email").getValue(String.class);
+                            Log.d(TAG, "Password for Jerry: " + email);
+                            show.setText(email);
+                            break;  // 可能有多個使用者，我們找到 Jerry 就中斷迴圈
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
 
         }
         private View.OnClickListener lis=new View.OnClickListener() {
