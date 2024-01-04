2c7393eadb77062329b15aa9878a23aecb9a1341
feather1217 <609sophie@gmail.com>
2024-01-04 21:41:36 +0800

    飲食檢查弄好可以篩選了 請jerry努力

--- demo/src/main/java/com/example/demo/home_diet_inspection.java
+++ demo/src/main/java/com/example/demo/home_diet_inspection.java
@@ -4,17 +4,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class home_diet_inspection extends AppCompatActivity {
     private ImageView imgBack;
+    private Button btnsearch;
     private CheckBox che_fruit,che_bread,che_vegetable,che_oil,che_fish,che_milk;
+    private TextView show;
+    List<String> foodList=new ArrayList<>();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,15 +39,58 @@ public class home_diet_inspection extends AppCompatActivity {
         che_oil=findViewById(R.id.checkOil);
         che_fish=findViewById(R.id.checkFish);
         che_milk=findViewById(R.id.checkMilk);
+        btnsearch=findViewById(R.id.btnSearch);
+        show=findViewById(R.id.textView12);
 
 
+        che_fruit.setOnCheckedChangeListener(chk);
+        che_bread.setOnCheckedChangeListener(chk);
+        che_vegetable.setOnCheckedChangeListener(chk);
+        che_oil.setOnCheckedChangeListener(chk);
+        che_fish.setOnCheckedChangeListener(chk);
+        che_milk.setOnCheckedChangeListener(chk);
         imgBack.setOnClickListener(lis);
+        btnsearch.setOnClickListener(S);
+
     }
-    private View.OnClickListener lis=new View.OnClickListener() {
+    private View.OnClickListener S=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 
+            imgBack.setOnClickListener(lis);
+
+            show.setText(foodList.toString());
+        }
+    };
+    private View.OnClickListener lis=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
             finish();
+        }
+    };
+
+    private CompoundButton.OnCheckedChangeListener chk=new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            foodList.clear();
+            if(che_fruit.isChecked()){
+                foodList.add("水果類");
+            }
+            if(che_bread.isChecked()){
+                foodList.add("五榖根莖類");
+            }
+            if(che_vegetable.isChecked()){
+                foodList.add("蔬菜類");
+            }
+            if(che_oil.isChecked()){
+                foodList.add("油脂與堅果類");
+            }
+            if(che_fish.isChecked()){
+                foodList.add("蛋豆魚肉類");
+            }
+            if(che_milk.isChecked()){
+                foodList.add("乳品類");
+            }
         }
     };
 }
